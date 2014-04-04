88bb8e35820784314a0e2f88c922f9dcd811ad5c
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-04-04 16:59:47 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-people_app
-==========
+General Assembly WDI - Feb '14
+==============================
+Name: Jason Leibowitz
+Github: [jasonleibowitz](https://github.com/jasonleibowitz/people_app)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PeopleApp::Application.config.secret_key_base = '03b426f45a16228dfcef30d219a75780f7c2af27d37bf9362a43a662a58e3265fe6293710f6bd16eda5cd6d303ef4785146a922354692bea7c8661ff67eb0e84'
